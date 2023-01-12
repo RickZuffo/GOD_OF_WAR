d727a9bbad8747ec144ae6fbbb6a84e099e0625b
RickZuffo <rickzuffo@gmail.com>
2023-01-12 19:22:56 -0300

    final do projeto

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,34 @@
 var swiper = new Swiper(".slide-characters", {
-    slidesPerView: 3.5,
+    slidesPerView: 4,
     spaceBetween: 19,
-    freeMode: true,   
+    freeMode: true,  
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 19,
+      },
+      768: {
+        slidesPerView:1.5,
+        spaceBetween:19,
+      },
+      991:{
+        slidesPerView:2.5,
+        spaceBetween:19,
+      },
+      1200: {
+        slidesPerView:3,
+        spaceBetween:19,
+      },
+      1400: {
+        slidesPerView:4,
+        spaceBetween:19,
+      },
+    } 
   });
+
+  AOS.init({
+    duration: 2000,
+    once: true,
+  });
+
+  
